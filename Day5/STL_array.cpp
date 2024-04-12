616c533a1b853961ef50c26505f079d3dcfb03e9
VINODvoid <iamlearner.v@gmail.com>
2024-04-12 19:44:12 +0530

    fix: added comment

--- Day5/STL_array.cpp
+++ Day5/STL_array.cpp
@@ -1,4 +1,5 @@
 // ? STL is a library in C++ that provides a set of common template classes and functions to work with different data structures, vectors, lists, queues, etc.
+// ? STL array is a static array that is introduced in C++11 standard. It is a container that encapsulates fixed size arrays.
 #include <iostream>
 #include <array>
 using namespace std;
@@ -25,7 +26,7 @@ int main()
     cout<<"\nArray is empty or not: "<<arr.empty(); //* returns 0 if not empty and 1 if empty
     cout<<"\nFirst element of the array is: "<<arr.front();
     cout<<"\nLast element of the array is: "<<arr.back();
-    
+
 
     return 0;
 
