ad2f3dab6be554efffe7e8c9e585a2a0e082d6de
VINODvoid <iamlearner.v@gmail.com>
2024-04-12 17:25:07 +0530

    CI:"Insertion sort"

---
+++ Day4/InsertionSort.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+void InsertionSort(int n,int arr[])
+{
+    for (int i = 0; i <n; i++)
+    {
+        int temp = arr[i];int j=i-1;
+        for(;j>=0;j--)
+        {
+            if(arr[j]>temp)
+            {
+                arr[j+1]= arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1] = temp;
+    }
+    
+
+}
+int main()
+{
+    int n, arr[10];
+    cout<<"Enter the size of the array: ";
+    cin >> n;
+    cout<<"Enter the elemets of the array" << endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"Unsorted Array is: " ;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i] << " ";
+    }
+    cout<<"\nSorted Array is: ";
+    InsertionSort(n,arr);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i] << " ";
+    }
+    return 0;
+}
