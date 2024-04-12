d9b5f3c2b9925a31a6a3a9c4f20b5d74264b986d
VINODvoid <iamlearner.v@gmail.com>
2024-04-12 19:40:53 +0530

    Ci : stl_array

---
+++ Day5/STL_array.cpp
@@ -0,0 +1,32 @@
+// ? STL is a library in C++ that provides a set of common template classes and functions to work with different data structures, vectors, lists, queues, etc.
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    array <int , 5> arr; //* STL method to declare an array
+    cout<<"Enter the elemets of the array" << endl;
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Array is: ";
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i] << " ";
+    }
+    int size = arr.size();
+    cout<<"\nSize of the array is: "<<size;
+
+    //* for accessing the elements of the array in STL
+    cout<<"\nElement at index 2 is: "<< arr.at(2);
+
+    cout<<"\nArray is empty or not: "<<arr.empty(); //* returns 0 if not empty and 1 if empty
+    cout<<"\nFirst element of the array is: "<<arr.front();
+    cout<<"\nLast element of the array is: "<<arr.back();
+    
+
+    return 0;
+
+}
